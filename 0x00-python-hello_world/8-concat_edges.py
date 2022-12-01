7fafbdfcc4d931d879438819949745f18605a464
HusamDin <hossamn069@gmail.com>
2022-12-01 12:42:06 +0000

    Print object-oriented programming with Python with the same variable

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,5 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 print(f"{str[39:75]} with Python")
+print(f"{str[39:75]}{str[106:111]} {str[:6]}")
 print(str)
