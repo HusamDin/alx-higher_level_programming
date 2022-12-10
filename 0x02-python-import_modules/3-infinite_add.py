fbc983cb83b0dfca9180f999227883838edc70ce
HusamDin <hossamn069@gmail.com>
2022-12-10 05:28:01 +0000

    Prints the result of the addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    """"Print the result of the addition of all arguments"""
+    from sys import argv
+
+    result = 0
+
+    if len(argv) == 1:
+        print(result)
+
+    if len(argv) > 1:
+        for i in range(1, len(argv)):
+            result += int(argv[i])
+
+        print(result)
+        
