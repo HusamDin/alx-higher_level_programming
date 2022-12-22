de98441556e595525b4b3779522e9ce75742b621
HusamDin <hossamn069@gmail.com>
2022-12-22 12:18:36 +0000

    Raise a type exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     """Print x elements of a list.
-   
+
     Args:
         my_list (list): The list to print elements from.
         x (int): The number of elements of my_list to print.
@@ -14,7 +14,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end='')
             res += 1
-        except:
-            break;
+        except IndexError:
+            break
     print()
     return (res)
