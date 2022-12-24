f08531833a3e92052a84e95b4c4e72aa1a6bc44a
HusamDin <hossamn069@gmail.com>
2022-12-24 19:20:20 +0000

    Define a square by: (based on 1-square.py)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,7 +8,8 @@ Instantiation with optional size with conditions if provided.
 class Square:
     """Represents a square with Private instance attribute: size."""
     def __init__(self, size=0):
-        if type(size) == int:
+#        if type(size) == int:
+        if isinstance(size, int):
             self.__size = size
         else:
             raise TypeError('size must be an integer')
