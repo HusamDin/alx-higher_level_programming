4cdee194b6d4a02505e82cd117c14ac2204e65c4
HusamDin <hossamn069@gmail.com>
2022-12-27 16:19:28 +0000

    Print a # square with coordinates

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -13,10 +13,10 @@ class Square:
         else:
             self.__size = size
 
-        if(not isinstance(position, tuple) or
-               len(position) != 2 or
-               not all(isinstance(num, int) for num in position) or
-               not all(num >= 0 for num in position)):
+        if (not isinstance(position, tuple) or
+               len(position) != 2 or not
+               all(isinstance(num, int) for num in position) or not
+               all(num >= 0 for num in position)):
             raise TypeError('position must be a tuple of 2 positive integers')
         else:
             self.__position = position
