4cad0a4f8cee04d46cca9a708a39ad29112477c7
HusamDin <hossamn069@gmail.com>
2023-01-04 09:03:11 +0000

    Rectangle that defines a rectangle by: (based on 2-rectangle.py)

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -4,8 +4,8 @@
 class Rectangle:
     """Class Rectangle that defines a rectangle by: (based on 3-rectangle.py"""
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
