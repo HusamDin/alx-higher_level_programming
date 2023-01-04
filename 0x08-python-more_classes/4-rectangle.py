1980432ec32ea4ffde3dcbdaa61abe34ec863044
HusamDin <hossamn069@gmail.com>
2023-01-04 20:14:27 +0000

    Rectangle that defines a rectangle by: (based on 3-rectangle.py)

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -39,7 +39,7 @@ class Rectangle:
 
     def perimeter(self):
         if self.width == 0 or self.height == 0:
-            return 0
+            return (0)
         return self.width * 2 + self.height * 2
 
     def __str__(self):
