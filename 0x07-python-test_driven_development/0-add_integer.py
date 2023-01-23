da35fcb56fd52d9511c13b98479bda9abda87974
HusamDin <hossamn069@gmail.com>
2023-01-23 05:13:56 +0000

    My first doctest

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" 
+
+    This module has one function to add up two integers 
+
+"""
+
+def add_integer(a, b=98):
+    """Return the sum of two integers or floats as integers
+
+    Args:
+        a: first argument
+        b: second argument
+
+    Returns:
+        Sum of the two arguments
+
+    Raises:
+        TypeError: if either of the two arguments not an integer or a float
+    """
+    if isinstance(a, int) or isinstance(a, float):
+        if isinstance(b, int) or isinstance(b, float):
+            return (int(a) + int(b))
+        else:
+            raise TypeError('b must be an integer')
+    else:
+        raise TypeError('a must be an integer')
