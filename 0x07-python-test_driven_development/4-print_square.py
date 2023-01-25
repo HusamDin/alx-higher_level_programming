dd9be32358642b1a24706545bdd61b70abab0bdd
HusamDin <hossamn069@gmail.com>
2023-01-25 05:22:48 +0000

    Print a square with doctest

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+
+    This module has one function to print a square
+
+"""
+
+
+def print_square(size):
+    """
+
+    This function prints a square of size size
+
+    Args:
+        size: an int
+    
+    Raises:
+        TypeError: if size is not an int
+        ValueError: if size is less than 0
+
+    """
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    for i in range(size):
+        print('#' * size)
+    
