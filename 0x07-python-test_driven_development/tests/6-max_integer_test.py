ecd116a56b9d2bddd1087dcb414e182b337fae51
HusamDin <hossamn069@gmail.com>
2023-01-25 16:50:09 +0000

    Find the max int in a list and unittest it

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -23,6 +23,18 @@ class TestMaxInteger(unittest.TestCase):
         neg_list = [-5, -8, -1, -3]
         self.assertEqual(max_integer(neg_list), -1)
 
+        # Testing our function with a list with max at the end
+        end_max = [2, 1, 3, 5, 11]
+        self.assertEqual(max_integer(end_max), 11)
+
+        # Testing our function with a list with max at the beginning
+        beg_max = [10, 6, 2, 5, 7, 9]
+        self.assertEqual(max_integer(beg_max), 10)
+
+        # Testing our function with a list with max at the beginning
+        single_list = [1]
+        self.assertEqual(max_integer(single_list), 1)
+
     def test_max_integer_empty(self):
         # Testing our function with a list of 0 ints
         emp_list = []
