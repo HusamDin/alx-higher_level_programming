8b3f262e7e6a8a5a914ba1584429e0938dfbb0d3
HusamDin <hossamn069@gmail.com>
2023-02-06 05:50:16 +0000

    Replace all occurrences of an element by another in a new list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """
-This module has one function to replace all occurences of an element by another 
+This module has one function to replace all occurences of an element by another
 
 """
 
 def search_replace(my_list, search, replace):
     """
     This function replaces all occurences of an element by another in a new list
-    
-    Args: 
+
+    Args:
         my_list: the initial list
         search: the element to replace in the list
         replace: the new element in the list
     """
 
     new_list = my_list.copy()
-    
+
     for i in range(len(new_list)):
         if new_list[i] == search:
             new_list[i] = replace
 
     return (new_list)
- 
+
