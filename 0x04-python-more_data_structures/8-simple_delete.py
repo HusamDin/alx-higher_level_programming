4210f85a6a1c0c7119af53cd3075dd960dd1f3f4
HusamDin <hossamn069@gmail.com>
2023-02-06 13:03:08 +0000

    Delete a key in a dic

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+
+This module has one function to deletea key in a dic
+
+"""
+
+
+def simple_delete(a_dictionary, key=""):
+    """
+    Deletes a key in a dictionary
+    
+    Args:
+        a_dictionary: A dictionary
+        key: The key to be deleted
+
+    Return:
+        void
+    """
+    if key in a_dictionary:
+        del a_dictionary[key]
+
+    return a_dictionary
