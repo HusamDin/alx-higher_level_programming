a011b959eefa1607dc21181c11c75f1c1ce44c5c
HusamDin <hossamn069@gmail.com>
2023-02-06 14:15:17 +0000

    Multiply all dictionary values by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+
+This module has one function return a new dictionary with all values multiplied by 2
+
+"""
+def multiply_by_2(a_dictionary):
+    """
+    Return a new dictionary with all values multiplied by 2
+    
+    Args:
+        a_dictionary: Our target dic
+    
+    Return: new_dic
+    """
+    new_dic = a_dictionary.copy()
+
+    for k, v in new_dic.items():
+        new_dic[k] = v * 2
+
+    return (new_dic)
