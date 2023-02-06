24e0f458efe3cddb036dea1fa8875a3310977731
HusamDin <hossamn069@gmail.com>
2023-02-06 12:54:37 +0000

    Replace or add key/value in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+
+This module has one function replaces or adds key/value in a dictionary. 
+
+"""
+
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    Replaces or adds key/value pairs
+
+    Args:
+        a_dictionary: A dic
+        key: Always a key string
+        value: Can be any type
+
+    Return:
+        new_dic: An updated dic
+    """
+
+    for k, v in a_dictionary.items():
+        if k not in a_dictionary:
+            a_dictionary[key] = value
+
+    a_dictionary[key] = value
+    return (a_dictionary)
