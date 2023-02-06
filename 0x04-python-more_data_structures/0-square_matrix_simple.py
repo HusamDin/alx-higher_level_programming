543b4cd00b426ddee79a1408da21ad32c1dc3d95
HusamDin <hossamn069@gmail.com>
2023-02-06 05:46:32 +0000

    computes the square value of all integers of a matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" 
+
+This module has one function to compute the square value of a matrix values 
+
+"""
+
+def square_matrix_simple(matrix=[]):
+    """ 
+    This function computes the square value of a matrix values 
+    
+    Args: matrix - a 2D matrix
+
+    Returns: A new 2D square matrix bassed off matrix elements
+
+    """
+
+    mat = []
+
+    for i in range(len(matrix)):
+        mat.append([])
+        for j in range(len(matrix[i])):
+            mat[i].append(matrix[i][j] ** 2)
+    return (mat)
+
+
